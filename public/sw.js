664e2859ff84fa827c5b546380e41e7b5a0bd7a9
David <pie_masters@hotmail.co.uk>
2019-03-28 11:05:10 +0000

    Cleanup and added precaching

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,13 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.add('/src/js/app.js')
+      })
+  )
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,6 +16,5 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
   event.respondWith(fetch(event.request));
 });
