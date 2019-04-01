dd077c80479215cd35f045a53b6a12dde54f9b6e
David <pie_masters@hotmail.co.uk>
2019-04-01 11:25:33 +0100

    Added initial firebase support

--- public/sw.js
+++ public/sw.js
@@ -67,10 +67,9 @@ function isInArray(string, array) {
   }
   return array.indexOf(cachePath) > -1;
 }
-
 self.addEventListener('fetch', function (event) {
 
-  var url = 'https://httpbin.org/get';
+  var url = 'https://pwagram-d7056.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
       caches.open(CACHE_DYNAMIC_NAME)
