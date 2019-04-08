e63eddb50dca57f7b01405942fa0457e80b2243f
David <pie_masters@hotmail.co.uk>
2019-04-08 10:24:57 +0100

    Added notification options

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -23,13 +23,32 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
+function displayConfirmNotification() {
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png'
+    };
+
+    navigator.serviceWorker.ready
+        .then(function(swreg) {
+          swreg.showNotification('Successfully subscribed (from SW)!', options);
+        });
+  }
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
     console.log('User Choice', result);
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-
+      displayConfirmNotification();
     }
   });
 }
