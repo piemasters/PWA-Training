ec488b89609cca88c6b44ac47e8764bbee86108b
David <pie_masters@hotmail.co.uk>
2019-04-08 11:39:54 +0100

    Push subscription support and push messages sent from server

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -13,19 +14,37 @@ admin.initializeApp({
     databaseURL: 'https://pwagram-d7056.firebaseio.com/'
 });
 
-exports.storePostData = functions.https.onRequest(function(request, response) {
-    cors(request, response, function() {
-        admin.database().ref('posts').push({
-            id: request.body.id,
-            title: request.body.title,
-            location: request.body.location,
-            image: request.body.image
-        })
-            .then(function() {
-                response.status(201).json({message: 'Data stored', id: request.body.id});
-            })
+exports.storePostData = functions.https.onRequest(function (request, response) {
+  cors(request, response, function () {
+    admin.database().ref('posts').push({
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image
+    })
+      .then(function () {
+        webpush.setVapidDetails('mailto:test@test.com', 'BN6r36LRLDYbguP8P6w2eU8eB4jNXrkSV775LI_cDAV3Plip_39CtNLqSbCF4sqkffV0ko-ccvQSm735aUUM1V8', 'aan1n9Q6K9dWFJ5QyzeM1aj3Zr7P_LrufkIIvzwowQs');
+        return admin.database().ref('subscriptions').once('value');
+      })
+      .then(function (subscriptions) {
+        subscriptions.forEach(function (sub) {
+          var pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh
+            }
+          };
+
+          webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post added!'}))
             .catch(function(err) {
-                response.status(500).json({error: err});
-            });
-    });
+              console.log(err);
+            })
+        });
+        response.status(201).json({message: 'Data stored', id: request.body.id});
+      })
+      .catch(function (err) {
+        response.status(500).json({error: err});
+      });
+  });
 });
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -24,22 +24,72 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 function displayConfirmNotification() {
-  if ('serviceWorker' in navigator) {
-    var options = {
+    if ('serviceWorker' in navigator) {
+      var options = {
       body: 'You successfully subscribed to our Notification service!',
       icon: '/src/images/icons/app-icon-96x96.png',
       image: '/src/images/sf-boat.jpg',
       dir: 'ltr',
       lang: 'en-US', // BCP 47,
       vibrate: [100, 50, 200],
-      badge: '/src/images/icons/app-icon-96x96.png'
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+      ]
     };
 
     navigator.serviceWorker.ready
-        .then(function(swreg) {
-          swreg.showNotification('Successfully subscribed (from SW)!', options);
-        });
+      .then(function(swreg) {
+        swreg.showNotification('Successfully subscribed!', options);
+      });
   }
+}
+
+function configurePushSub() {
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+
+    var reg;
+    navigator.serviceWorker.ready
+        .then(function(swreg) {
+            reg = swreg;
+            return swreg.pushManager.getSubscription();
+        })
+        .then(function(sub) {
+            if (sub === null) {
+                // Create a new subscription
+                var vapidPublicKey = 'BN6r36LRLDYbguP8P6w2eU8eB4jNXrkSV775LI_cDAV3Plip_39CtNLqSbCF4sqkffV0ko-ccvQSm735aUUM1V8';
+                var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+                return reg.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidPublicKey
+                });
+            } else {
+                // We have a subscription
+            }
+        })
+        .then(function(newSub) {
+            return fetch('https://pwagram-d7056.firebaseio.com/subscriptions.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newSub)
+            })
+        })
+        .then(function(res) {
+            if (res.ok) {
+                displayConfirmNotification();
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
 
 function askForNotificationPermission() {
@@ -48,12 +98,13 @@ function askForNotificationPermission() {
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+        configurePushSub();
+        // displayConfirmNotification();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
